15a649166055e3aeea2757a4261baa16e0a76716
By-George <jorge20plus@gmail.com>
2022-05-16 16:41:12 -0500

    se agrego un comentario para la funcion de borrar carpetas

--- clase4/modulos/carpetas.py
+++ clase4/modulos/carpetas.py
@@ -6,6 +6,7 @@ def crear_carpetas(sufijo, cantidad):
         os.mkdir(f"{sufijo}{i+1}")
         print(f"Creando carpeta: {sufijo}{i+1}")
 
+#funcion para borrar carpetas
 def borrar_carpetas(sufijo, cantidad):
     for i in range(cantidad):
         os.rmdir(f"{sufijo}{i+1}")
