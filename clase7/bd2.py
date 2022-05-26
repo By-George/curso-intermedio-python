e3c6963d253b3dd398473734868aff31bfc07673
By-George <jorge20plus@gmail.com>
2022-05-25 20:56:26 -0500

    Se crea el archivo bd2, con la prueba de consultar un registro y tambien enlistar todos los usuarios de la tabla usuarios

--- clase7/bd2.py
+++ clase7/bd2.py
@@ -1,5 +1,8 @@
 #ejemplo de conexion a postgresql con psycopg2
 import psycopg2
+import os
+
+os.system('cls')
 
 conn = psycopg2.connect("dbname=python_intermedio_db user=postgres password=S3S3Sp22*")
 
@@ -7,8 +10,17 @@ conn = psycopg2.connect("dbname=python_intermedio_db user=postgres password=S3S3
 cur = conn.cursor()
 
 # Execute a command: this creates a new table
-cur.execute("SELECT * FROM usuarios;")
+cur.execute("SELECT * FROM usuarios WHERE id = 50;")
 print(cur.fetchone())
+
+print('-' * 70)
+
+cur.execute("SELECT * FROM usuarios;")
+#print(cur.fetchall())
+usuarios = cur.fetchall()
+
+for usuario in usuarios:
+    print(usuario)
 
 #Make the changes to the database persistent
 conn.commit()
